8f0ba0779c08e5e844b8174a3532b476f95b2fdf
Crowdbotics <team@crowdbotics.com>
2023-03-16 06:13:26 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mute-mountain-39416",
+  url: "https://mute-mountain-39416.botics.co",
+  api: "https://mute-mountain-39416.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
